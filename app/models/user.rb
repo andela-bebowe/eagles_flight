0fe90565d675f1d1e94833be38bed8057dacac46
Ebowe Blessing <blessing.ebowe@andela.com>
2015-10-20 13:08:41 +0000

    Included a home page with a static page controller.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+	before_save {self.email = email.downcase}
+	has_secure_password
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :name, presence: true, length: {maximum: 50, minimum: 3}
+	validates :email, presence: true, length: {maximum: 100, minimum: 4}, uniqueness: {case_sensitive: false}, format: {with: VALID_EMAIL_REGEX}
 end
